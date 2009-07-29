a9e16ea1fb29725f091fb28b45cacbd532095c7c
Norman Clarke <norman@njclarke.com>
2009-07-29 12:25:46 -0300

    Cleaned up grammar rules.

--- haml/lexer.lua
+++ haml/lexer.lua
@@ -1,55 +1,70 @@
 --- Haml Lexer
 module("haml.lexer", package.seeall)
+
+-- Lua Haml's lexer uses the Lua Parsing Expression Grammar. For more 
+-- information on how to read and understand this code, see: 
+-- http://www.inf.puc-rio.br/~roberto/lpeg/lpeg.html
 require "lpeg"
 
 local P, S, R, C, Cg, Ct, V = lpeg.P, lpeg.S, lpeg.R, lpeg.C, lpeg.Cg, lpeg.Ct, lpeg.V
 
-local script_operator = Cg(P"=", "script_operator")
-local escape_operator = Cg(P"\\", "escape_operator")
-local doctype_operator = Cg(P"!!!", "doctype_operator")
-local markup_comment_operator = Cg(P"/", "markup_comment_operator")
-local tag_indicator = P"%"
-local self_closing_modifier = Cg(P"/", "self_closing_modifier")
-local inner_whitespace_modifier = Cg(P"<", "inner_whitespace_modifier")
-local outer_whitespace_modifier = Cg(P">", "outer_whitespace_modifier")
 local leading_whitespace = Cg(S" "^0 / string.len, "space")
-local inline_whitespace = S" \t"
-local eol = S"\n" + S"\r\n" + S"\r"
-local markup_tag = Cg(R("az", "AZ") * (R("az", "AZ", "09") + S("-:_"))^0, "markup_tag")
--- note that this is not valid CSS, but Haml allows it
-local css_ident = (P"_" + R("az", "AZ", "09"))^1 * (S"-_" + R("az", "AZ", "09"))^0
-local css_id = P"#" * Cg(css_ident^1, "css_id")
+local inline_whitespace  = S" \t"
+local eol                = S"\n" + S"\r\n" + S"\r"
+local empty_line         = Cg(P(""), "empty_line")
+local unparsed           = Cg((1 - eol)^1, "unparsed")
+
+-- Haml operators
+local operators = {
+  tag            = P"%",
+  script         = Cg(P"=",   "script_operator"),
+  escape         = Cg(P"\\",  "escape_operator"),
+  doctype        = Cg(P"!!!", "doctype_operator"),
+  markup_comment = Cg(P"/",   "markup_comment_operator")
+}
+
+-- Modifiers that follow Haml markup tags
+local modifiers = {
+  self_closing     = Cg(P"/", "self_closing_modifier"),
+  inner_whitespace = Cg(P"<", "inner_whitespace_modifier"),
+  outer_whitespace = Cg(P">", "outer_whitespace_modifier")
+}
+
+-- Character sequences for CSS and XML/HTML elements. Note that many invalid
+-- names are allowed because of Haml's flexibility.
+local markup_tag  = Cg(R("az", "AZ") * (R("az", "AZ", "09") + S("-:_"))^0, "markup_tag")
+local css_ident   = (P"_" + R("az", "AZ", "09"))^1 * (S"-_" + R("az", "AZ", "09"))^0
+local css_id      = P"#" * Cg(css_ident^1, "css_id")
 local css_classes = P"." * Cg((css_ident + P".")^1, "css_classes")
-local poratable_style_attributes = P{"(" * ((1 - S"()") + V(1))^0 * ")"}
-local lua_style_attributes = P{"{" * ((1 - S"{}") + V(1))^0 * "}"}
-local any_attributes = Cg(poratable_style_attributes + lua_style_attributes)
-local attributes = Cg(Ct(any_attributes * any_attributes^0), "attributes")
+
+-- Markup attributes
+local paren_attributes = P{"(" * ((1 - S"()") + V(1))^0 * ")"}
+local brace_attributes = P{"{" * ((1 - S"{}") + V(1))^0 * "}"}
+local any_attributes   = Cg(paren_attributes + brace_attributes)
+local attributes       = Cg(Ct(any_attributes * any_attributes^0), "attributes")
 
 -- Haml HTML elements
 local css_id_and_classes = ((css_id^1 * css_classes^0) + (css_classes^1 * css_id^0))
-local explicit_tag = (tag_indicator * markup_tag * css_id_and_classes^0)
-local tag_modifiers = (
-  self_closing_modifier + 
-  (inner_whitespace_modifier + outer_whitespace_modifier)
-)
-local haml_tag = (explicit_tag + css_id_and_classes)
-local unparsed = Cg((1 - eol)^1, "unparsed")
+local explicit_tag       = (operators["tag"] * markup_tag * css_id_and_classes^0)
+local tag_modifiers      = (modifiers["self_closing"] + (modifiers["inner_whitespace"] + modifiers["outer_whitespace"]))
+local haml_tag           = (explicit_tag + css_id_and_classes)
+
+-- Core Haml grammar
 local haml_element = leading_whitespace * (
+  -- Haml markup
+  (haml_tag * attributes^0 * tag_modifiers^-1 * operators["script"]^0 * unparsed^0) +
   -- doctype or prolog
-  (doctype_operator * (inline_whitespace^1 * unparsed)^0) +
-  -- HAML markup
-  (haml_tag * attributes^0 * tag_modifiers^-1 * script_operator^0 * unparsed^0) +
+  (operators["doctype"] * (inline_whitespace^1 * unparsed)^0) +
   -- Markup comment
-  (markup_comment_operator * unparsed^0) +
+  (operators["markup_comment"] * unparsed^0) +
   -- Escaped
-  (escape_operator * unparsed^0) +
+  (operators["escape"] * unparsed^0) +
   -- Unparsed content
   unparsed +
-  -- Empty line
-  Cg(P(""), "empty_line")
+  -- Last resort
+  empty_line
 )
-local chunk = Ct(haml_element)
-local grammar = Ct(chunk * (eol * chunk)^0)
+local grammar = Ct(Ct(haml_element) * (eol * Ct(haml_element))^0)
 
 function tokenize(input)
   return lpeg.match(grammar, input)
