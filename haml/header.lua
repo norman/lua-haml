f9c10a33970d9b0b5152e73d7b9e38c19f8e85e5
Norman Clarke <norman@njclarke.com>
2009-12-03 16:01:19 -0300

    Added support for "!!! 5".

--- haml/header.lua
+++ haml/header.lua
@@ -1,7 +1,16 @@
 module("haml.header", package.seeall)
 
+--- The HTML4 doctypes; default is 4.01 Transitional.
+html_doctypes = {
+  ["5"]    = '<!DOCTYPE html>',
+  STRICT   = '<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">',
+  FRAMESET = '<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN" "http://www.w3.org/TR/html4/frameset.dtd">',
+  DEFAULT  = '<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">'
+}
+
 --- The XHTML doctypes; default is 1.0 Transitional.
 xhtml_doctypes = {
+  ["5"]    = html_doctypes["5"],
   STRICT   = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">',
   FRAMESET = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Frameset//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd">',
   MOBILE   = '<!DOCTYPE html PUBLIC "-//WAPFORUM//DTD XHTML Mobile 1.2//EN" "http://www.openmobilealliance.org/tech/DTD/xhtml-mobile12.dtd">',
@@ -9,15 +18,10 @@ xhtml_doctypes = {
   DEFAULT  = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">'
 }
 
---- The HTML4 doctypes; default is 4.01 Transitional.
-html4_doctypes = {
-  STRICT   = '<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">',
-  FRAMESET = '<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN" "http://www.w3.org/TR/html4/frameset.dtd">',
-  DEFAULT  = '<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">'
-}
 
 --- Returns an XML prolog for the precompiler state.
 local function prolog_for(state)
+  if state.options.format:match("^html") then return nil end
   local charset = state.curr_phrase.charset or state.options.encoding
   state.buffer:string(string.format("<?xml version='1.0' encoding='%s' ?>", charset), {newline = true})
 end
@@ -26,7 +30,7 @@ end
 local function doctype_for(state)
 
   if state.options.format == 'html5' then
-    return state.buffer:string('<!DOCTYPE html>', {newline = true})
+    return state.buffer:string(html_doctypes["5"], {newline = true})
 
   elseif state.curr_phrase.version == "1.1" then
     return state.buffer:string('<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">', {newline = true})
@@ -36,7 +40,7 @@ local function doctype_for(state)
     return state.buffer:string(doctype, {newline = true})
 
   elseif state.options.format == 'html4' then
-    local doctype = html4_doctypes[state.curr_phrase.doctype] or html4_doctypes.DEFAULT
+    local doctype = html_doctypes[state.curr_phrase.doctype] or html_doctypes.DEFAULT
     return state.buffer:string(doctype, {newline = true})
 
   else
